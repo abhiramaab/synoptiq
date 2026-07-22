ce39a0f9975e8c1cabd5e248a4e4010b32011bc0
Abhiram A B <abhiram.b@icloud.com>
2026-07-23 00:31:43 +0530

    Test OAuth2UserService execution

--- src/main/java/ai/synoptiq/oauth/CustomOAuth2UserService.java
+++ src/main/java/ai/synoptiq/oauth/CustomOAuth2UserService.java
@@ -30,51 +30,11 @@ public void init() {
     System.out.println(">>> CustomOAuth2UserService bean created");
 }
 
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest request)
-            throws OAuth2AuthenticationException {
 
-        System.out.println("########################################");
-        System.out.println("######## CUSTOM OAUTH SERVICE ##########");
-        System.out.println("########################################");
+@Override
+public OAuth2User loadUser(OAuth2UserRequest request)
+        throws OAuth2AuthenticationException {
 
-        OAuth2User oauthUser = super.loadUser(request);
-
-        Map<String, Object> attributes = oauthUser.getAttributes();
-
-        String email = (String) attributes.get("email");
-        String name = (String) attributes.get("name");
-        String googleId = (String) attributes.get("sub");
-        String picture = (String) attributes.get("picture");
-
-        User user = userRepository.findByEmail(email)
-                .orElseGet(User::new);
-
-        user.setEmail(email);
-        user.setUsername(name);
-        user.setGoogleId(googleId);
-        user.setProfilePicture(picture);
-        user.setRole(Role.USER);
-        user.setProvider(Provider.GOOGLE);
-
-        if (user.getCreatedAt() == null) {
-            user.setCreatedAt(LocalDateTime.now());
-        }
-
-        user.setUpdatedAt(LocalDateTime.now());
-
-        user = userRepository.saveAndFlush(user);
-
-        System.out.println("================================");
-        System.out.println("USER SAVED SUCCESSFULLY");
-        System.out.println("ID    : " + user.getId());
-        System.out.println("EMAIL : " + user.getEmail());
-        System.out.println("================================");
-
-        return new DefaultOAuth2User(
-                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
-                attributes,
-                "email"
-        );
-    }
+    throw new RuntimeException("CUSTOM OAUTH SERVICE CALLED");
+}
 }
